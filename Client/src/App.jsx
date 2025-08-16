430dd964cd31762df887bbcb61578f7381306310
raghav-webseeder <mishra.raghav@webseeder.tech>
2025-08-16 16:59:42 +0530

    Added Backend URL

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -128,7 +128,7 @@ const App = () => {
     const username = result.value;
     setPlayerName(username);
 
-    const newSocket = io("http://localhost:3000", {
+    const newSocket = io("https://tic-tac-toe-12ah.onrender.com", {
       autoConnect: true,
     });
 
